f31f389795280f409c40a30267dc47ec4c31e842
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-04-30 11:03:23 -0300

    chore: update mod
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- go.mod
+++ go.mod
@@ -3,6 +3,29 @@ module github.com/fzipi/ftw-tests-yaml-schema
 go 1.19
 
 require (
-	github.com/projectdiscovery/yamldoc-go v1.0.3 // indirect
+	github.com/coreruleset/go-ftw v0.4.9
+	github.com/magefile/mage v1.14.0
+	github.com/projectdiscovery/yamldoc-go v1.0.3
+)
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/goccy/go-yaml v1.8.10 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	github.com/yargevad/filepathx v1.0.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- types.go
+++ types.go
@@ -1,6 +1,41 @@
 //go:generate docgen types.go types_doc.go FTWTest
 package main
 
+import (
+	"github.com/coreruleset/go-ftw/ftwhttp"
+	"github.com/coreruleset/go-ftw/test"
+)
+
+var (
+	destaddr = "192.168.0.1"
+	port     = 8080
+	protocol = "http"
+	uri      = "/test"
+	method   = "REPORT"
+	version  = "HTTP/1.1"
+
+	exampleInput = test.Input{
+		DestAddr:       &destaddr,
+		Port:           &port,
+		Protocol:       &protocol,
+		URI:            &uri,
+		Version:        &version,
+		Headers:        make(ftwhttp.Header),
+		Method:         &method,
+		Data:           nil,
+		EncodedRequest: "TXkgRGF0YQo=",
+		SaveCookie:     false,
+		StopMagic:      false,
+	}
+	exampleOutput = test.Output{
+		Status:           []int{200},
+		ResponseContains: "",
+		LogContains:      "nothing",
+		NoLogContains:    "",
+		ExpectError:      true,
+	}
+)
+
 // Welcome to the FTW YAMLFormat documentation.
 // In this document we will explain all the possible options that can be used within the YAML format.
 // Generally this is the preferred format for writing tests in as they don't require any programming skills
@@ -9,90 +44,98 @@ package main
 // FTWTest is the base type used when unmarshaling YAML tests files
 type FTWTest struct {
 	// description: |
-	// Meta describes the metadata information of this yaml test file
+	//   Meta describes the metadata information of this yaml test file
 	Meta Meta `yaml:"meta"`
 
 	// description: |
-	// FileName is the name of the file where these tests are.
+	//   FileName is the name of the file where these tests are.
 	// examples:
 	//   - name: FileName
-	//     value: test-1234.yaml
+	//     value: "\"test-1234.yaml\""
 	FileName string `yaml:"filename"`
+
 	// description: |
-	// Tests is a list of FTW tests
+	//   Tests is a list of FTW tests
 	// examples:
 	//   - name: Tests
-	//     value: the tests
+	//     value: "\"the tests\""
 	Tests []Test `yaml:"tests"`
 }
 
 // Meta describes the metadata information of this yaml test file
 type Meta struct {
 	// description: |
-	// Author is the list of authors that added content to this file
+	//   Author is the list of authors that added content to this file
 	// examples:
 	//   - name: Author
-	//     value: Felipe Zipitria
+	//     value: "\"Felipe Zipitria\""
 	Author string `yaml:"author,omitempty"`
+
 	// description: |
-	// Enabled indicates if the tests are enabled to be run by the engine or not.
+	//   Enabled indicates if the tests are enabled to be run by the engine or not.
 	// examples:
 	//   - name: Enabled
 	//     value: false
 	Enabled bool `yaml:"enabled,omitempty"`
+
 	// description: |
-	// Name is the name of the tests contained in this file.
+	//   Name is the name of the tests contained in this file.
 	// examples:
 	//   - name: Name
-	//     value: test01
+	//     value: "\"test01\""
 	Name string `yaml:"name,omitempty"`
+
 	// description: |
-	// Description is a textual description of the tests contained in this file.
+	//   Description is a textual description of the tests contained in this file.
 	// examples:
 	//   - name: Description
-	//     value: The tests here target SQL injection.
+	//     value: "\"The tests here target SQL injection.\""
 	Description string `yaml:"description,omitempty"`
+
 	// description: |
-	// Version is the version of the YAML Schema.
+	//   Version is the version of the YAML Schema.
 	// examples:
 	//   - name: Version
-	//     value: v1
+	//     value: "\"v1\""
 	Version string `yaml:"version,omitempty"`
 }
 
 // Test is an individual test
 type Test struct {
 	// description: |
-	// TestTitle the title of this particular test. It is used for inclusion/exclusion of each run by the tool.
+	//   TestTitle the title of this particular test. It is used for inclusion/exclusion of each run by the tool.
 	// examples:
 	//   - name: TestTitle
-	//     value: 920100-1
+	//     value: "\"920100-1\""
 	TestTitle string `yaml:"test_title"`
+
 	// description: |
-	// TestDescription is the description for this particular test. Should be used to describe the internals of
-	// the specific things this test is targeting.
+	//   TestDescription is the description for this particular test. Should be used to describe the internals of
+	//   the specific things this test is targeting.
 	// examples:
 	//   - name: TestDescription
-	//     value: This test targets something
+	//     value: "\"This test targets something\""
 	TestDescription string `yaml:"desc,omitempty"`
+
 	// description: |
-	// Stages is the list of all the stages to perform this test.
+	//   Stages is the list of all the stages to perform this test.
 	Stages []Stage `yaml:"stages"`
 }
 
 // Stage is an individual test stage
 type Stage struct {
 	// description: |
-	// Input is the data that is passed to the test
+	//   Input is the data that is passed to the test
 	// examples:
 	//   - name: Input
-	//     value: test
+	//     value: exampleInput
 	Input Input `yaml:"input"`
+
 	// description: |
-	// Output is the data that is returned from the test
+	//   Output is the data that is returned from the test
 	// examples:
 	//   - name: Output
-	//     value: test
+	//     value: exampleOutput
 	Output Output `yaml:"output"`
 }
 
@@ -100,109 +143,117 @@ type Stage struct {
 // The fields `Version`, `Method` and `URI` we want to explicitly now when they are set to ""
 type Input struct {
 	// description: |
-	// DestAddr is the IP of the destination host that the test will send the message to.
+	//   DestAddr is the IP of the destination host that the test will send the message to.
 	// examples:
 	//   - name: DestAddr
-	//     value: "127.0.0.1"
+	//     value: "\"127.0.0.1\""
 	DestAddr *string `yaml:"dest_addr,omitempty" koanf:"dest_addr,omitempty"`
+
 	// description: |
-	// Port allows you to declare which port on the destination host the tests should connect to.
+	//   Port allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: Port
 	//     value: 80
 	Port *int `yaml:"port,omitempty" koanf:"port,omitempty"`
+
 	// description: |
-	// Protocol allows you to declare which port on the destination host the tests should connect to.
+	//   Protocol allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: Protocol
-	//     value: 80
+	//     value: "\"http\""
 	Protocol *string `yaml:"protocol,omitempty" koanf:"protocol,omitempty"`
+
 	// description: |
-	// URI allows you to declare which port on the destination host the tests should connect to.
+	//   URI allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: URI
-	//     value: "/get?hello=world"
+	//     value: "\"/get?hello=world\""
 	URI *string `yaml:"uri,omitempty" koanf:"uri,omitempty"`
+
 	// description: |
-	// Version it the HTTP version used.
+	//   Version it the HTTP version used.
 	// examples:
 	//   - name: Version
-	//     value: "1.1"
+	//     value: "\"1.1\""
 	Version *string `yaml:"version,omitempty" koanf:"version,omitempty"`
+
 	// description: |
-	// Headers is a map of headers to be sent.
-	// examples:
-	//   - name: Headers
-	//     value: list
-	//Headers ftwhttp.Header `yaml:"headers,omitempty" koanf:"headers,omitempty"`
-	// description: |
-	// Method allows you to declare which port on the destination host the tests should connect to.
+	//   Method allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: Method
-	//     value: "GET"
+	//     value: "\"GET\""
 	Method *string `yaml:"method,omitempty" koanf:"method,omitempty"`
+
 	// description: |
-	// Data allows you to declare which port on the destination host the tests should connect to.
+	//   Data allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: Data
-	//     value: "Bibitti bopi"
+	//     value: "\"Bibitti bopi\""
 	Data *string `yaml:"data,omitempty" koanf:"data,omitempty"`
+
 	// description: |
-	// SaveCookie allows you to declare which port on the destination host the tests should connect to.
+	//   SaveCookie allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: SaveCookie
 	//     value: 80
 	SaveCookie bool `yaml:"save_cookie,omitempty" koanf:"save_cookie,omitempty"`
+
 	// description: |
-	// StopMagic allows you to declare which port on the destination host the tests should connect to.
+	//   StopMagic allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: StopMagic
 	//     value: false
 	StopMagic bool `yaml:"stop_magic" koanf:"stop_magic,omitempty"`
+
 	// description: |
-	// EncodedRequest allows you to declare which port on the destination host the tests should connect to.
+	//   EncodedRequest allows you to declare which port on the destination host the tests should connect to.
 	// examples:
 	//   - name: EncodedRequest
-	//     value: "a"
+	//     value: "\"a\""
 	EncodedRequest string `yaml:"encoded_request,omitempty" koanf:"encoded_request,omitempty"`
+
 	// description: |
-	// RAWRequest is deprecated.
+	//   RAWRequest is deprecated.
 	// examples:
 	//   - name: RAWRequest
-	//     value: asd
+	//     value: "\"TXkgRGF0YQo=\""
 	RAWRequest string `yaml:"raw_request,omitempty" koanf:"raw_request,omitempty"`
 }
 
 // Output is the response expected from the test
 type Output struct {
 	// description: |
-	// Status describes the HTTP status error code expected as response.
+	//   Status describes the HTTP status error code expected as response.
 	// examples:
 	//   - name: Status
 	//     value: [200]
 	Status []int `yaml:"status,flow,omitempty"`
+
 	// description: |
-	// ResponseContains describes the text that should be contained in the HTTP response.
+	//   ResponseContains describes the text that should be contained in the HTTP response.
 	// examples:
 	//   - name: ResponseContains
-	//     value: "Hello, World"
+	//     value: "\"Hello, World\""
 	ResponseContains string `yaml:"response_contains,omitempty"`
+
 	// description: |
-	// LogContains describes the text that should be contained in the WAF logs.
+	//   LogContains describes the text that should be contained in the WAF logs.
 	// examples:
 	//   - name: LogContains
-	//     value: `id "920100"`
+	//     value: "\"id 920100\""
 	LogContains string `yaml:"log_contains,omitempty"`
+
 	// description: |
-	// NoLogContains describes the text that should be contained in the WAF logs.
+	//   NoLogContains describes the text that should be contained in the WAF logs.
 	// examples:
 	//   - name: NoLogContains
-	//     value: `id "920100"
+	//     value: "\"id 920100\""
 	NoLogContains string `yaml:"no_log_contains,omitempty"`
+
 	// description: |
-	// ExpectError describes the text that should be contained in the WAF logs.
+	//   When `ExpectError` is true, we don't expect an answer from the WAF, just an error.
 	// examples:
 	//   - name: ExpectError
-	//     value: `id "920100"
+	//     value: false
 	ExpectError bool `yaml:"expect_error,omitempty"`
 }
