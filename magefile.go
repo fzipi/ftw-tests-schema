ed1cf32e18f0c5c5689e9e96e0779e61b0a92c98
Felipe Zipitria <felipe.zipitria@owasp.org>
2023-11-09 10:37:42 +0100

    chore: update golanci-lint version
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- magefile.go
+++ magefile.go
@@ -16,7 +16,7 @@ import (
 )
 
 var addLicenseVersion = "v1.1.1" // https://github.com/google/addlicense
-var golangCILintVer = "v1.52.2"  // https://github.com/golangci/golangci-lint/releases
+var golangCILintVer = "v1.55.1"  // https://github.com/golangci/golangci-lint/releases
 var gosImportsVer = "v0.3.8"     // https://github.com/rinchsan/gosimports/releases/tag/v0.3.8
 
 var errRunGoModTidy = errors.New("go.mod/sum not formatted, commit changes")
